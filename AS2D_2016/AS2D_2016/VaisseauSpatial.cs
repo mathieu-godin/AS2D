59498e1e36fb95420243b09f6349b616a29c10cf
Raphaël Brulé <raphael_brule@hotmail.com>
2016-10-05 11:11:26 -0400

    //CLASSE TRÈS IMCOMPLÈTE!!

--- AS2D_2016/AS2D_2016/VaisseauSpatial.cs
+++ AS2D_2016/AS2D_2016/VaisseauSpatial.cs
@@ -15,17 +15,28 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-//CLASSE TRÈS IMCOMPLÈTES!!
+//CLASSE TRÈS IMCOMPLÈTE!!
 
-namespace AS2D_2016
+namespace AtelierXNA
 {
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
     public class VaisseauSpatial : SpriteAnimé
     {
+        //Propriété initialement gérée par le constructeur
         float IntervalleMAJDéplacement { get; set; }
 
+        /// <summary>
+        /// Constructeur de VaisseauSpatial
+        /// </summary>
+        /// <param name="jeu">Objet de classe Game</param>
+        /// <param name="nomImage">Nom de l'image (string)</param>
+        /// <param name="position">Position (Vector2)</param>
+        /// <param name="zoneAffichage">Zone d'affichage (Rectangle)</param>
+        /// <param name="descriptionImage">Description de l'image (Vector2)</param>
+        /// <param name="intervalleMAJAnimation">Intervalle de mise à jour de l'animation (float)</param>
+        /// <param name="intervalleMAJDéplacement">Intervalle de mise à jour du déplacement (float)</param>
         public VaisseauSpatial(Game jeu, string nomImage,
                                Vector2 position, Rectangle zoneAffichage,
                                Vector2 descriptionImage, float intervalleMAJAnimation,
