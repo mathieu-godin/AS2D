efd774c3f8583730adc47d48fe85e8715ab285b8
Raphaël Brulé <raphael_brule@hotmail.com>
2016-10-05 11:11:35 -0400

    //CLASSE TRÈS IMCOMPLÈTE!!

---
+++ AS2D_2016/AS2D_2016/Sphère.cs
@@ -0,0 +1,70 @@
+﻿/* Auteur :            Raphaël Brulé
+   Fichier :           Sphère.cs
+   Date :              le 05 octobre 2016
+   Description :       Ce component, enfant de SpriteAnimé, permet
+                       de gérer une sphère.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+//CLASSE TRÈS IMCOMPLÈTE!!
+
+namespace AtelierXNA
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Sphère : SpriteAnimé
+    {
+        //Propriété initialement gérée par le constructeur
+        float IntervalleMAJDéplacement { get; set; }
+
+        /// <summary>
+        /// Constructeur de Sphère
+        /// </summary>
+        /// <param name="jeu">Objet de classe Game</param>
+        /// <param name="nomImage">Nom de l'image (string)</param>
+        /// <param name="position">Position (Vector2)</param>
+        /// <param name="zoneAffichage">Zone d'affichage (Rectangle)</param>
+        /// <param name="descriptionImage">Description de l'image (Vector2)</param>
+        /// <param name="intervalleMAJAnimation">Intervalle de mise à jour de l'animation (float)</param>
+        /// <param name="intervalleMAJDéplacement">Intervalle de mise à jour du déplacement (float)</param>
+        public Sphère(Game jeu, string nomImage, Vector2 position,
+                      Rectangle zoneAffichage, Vector2 descriptionImage,
+                      float intervalleMAJAnimation, float intervalleMAJDéplacement)
+            : base(jeu, nomImage, position, zoneAffichage,
+                   descriptionImage, intervalleMAJAnimation)
+        {
+            IntervalleMAJDéplacement = intervalleMAJDéplacement;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
