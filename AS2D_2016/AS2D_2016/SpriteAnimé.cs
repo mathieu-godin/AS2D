4197e2c709503c2de058083e8d5adb1c210833c3
Mathieu <matgodin@videotron.ca>
2016-10-05 11:10:14 -0400

    SpriteAnimé

---
+++ AS2D_2016/AS2D_2016/SpriteAnimé.cs
@@ -0,0 +1,83 @@
+﻿/*
+SpriteAnimé.cs
+--------------
+
+Par Mathieu Godin
+
+Rôle : Composant qui hérite de Sprite et qui
+       permet d'animer le sprite qui sera
+       affiché à l'écran en défilant différent
+       dans la même image chargée
+
+Créé : 5 octobre 2016
+*/
+using Microsoft.Xna.Framework;
+
+namespace AtelierXNA
+{
+    /// <summary>
+    /// Composant qui peut afficher un sprite animé par un défilement de sprites présents sur la même image chargée
+    /// </summary>
+    public class SpriteAnimé : Sprite
+    {
+        const float AUCUN_DÉPLACEMENT = 0.0F;
+        const int ORIGINE = 0;
+        Vector2 Delta { get; set; }
+        Rectangle RectangleSource { get; set; }
+        Vector2 DescriptionImage { get; set; }
+        float IntervalleMAJAnnimation { get; set; }
+        float MargeDroite { get; set; }
+        float MargeBas { get; set; }
+
+        /// <summary>
+        /// Constructeur de la classe SpriteAnimé
+        /// </summary>
+        /// <param name="game">Jeu dde type Game</param>
+        /// <param name="nomImage">Nom du sprite tel qu'inscrit dans son dossier respectif</param>
+        /// <param name="position">Position de départ du sprite</param>
+        /// <param name="zoneAffichage">Zone d'affichage du sprite</param>
+        /// <param name="descriptionImage">Le nombres de sprites en x et en y contenus dans l'image chargée</param>
+        /// <param name="intervalleMAJAnimation">Intervalle de mise à jour de l'animation du sprite</param>
+        public SpriteAnimé(Game game, string nomImage, Vector2 position, Rectangle zoneAffichage, Vector2 descriptionImage, float intervalleMAJAnimation) : base(game, nomImage, position, zoneAffichage)
+        {
+            DescriptionImage = new Vector2(descriptionImage.X, descriptionImage.Y);
+            IntervalleMAJAnnimation = intervalleMAJAnimation;
+        }
+
+        /// <summary>
+        /// Méthode de chargement de contenu nécessaire au SpriteAnimé
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            RectangleSource = new Rectangle(ORIGINE, ORIGINE, (int)Delta.X, (int)Delta.Y);
+        }
+
+        /// <summary>
+        /// Calcule les marges du sprite
+        /// </summary>
+        protected void CalculerMarges()
+        {
+            Delta = new Vector2(Image.Width, Image.Height) / DescriptionImage;
+            MargeDroite = Game.Window.ClientBounds.Width - (int)Delta.X;
+            MargeBas = Game.Window.ClientBounds.Height - (int)Delta.Y;
+        }
+
+        /// <summary>
+        /// Méthode qui met à jour le SpriteAnimé selon le temps écoulé
+        /// </summary>
+        protected void EffectuerMiseÀJour()
+        {
+            RectangleSource = new Rectangle((RectangleSource.X + (int)Delta.X) % Image.Width, RectangleSource.X > Image.Width - (int)Delta.X ? (RectangleSource.Y > Image.Height - (int)Delta.Y ? ORIGINE : RectangleSource.Y + (int)Delta.Y) : RectangleSource.Y, (int)Delta.X, (int)Delta.Y);
+        }
+
+        /// <summary>
+        /// Méthode qui dessine le SpriteAnimé à l'écran
+        /// </summary>
+        /// <param name="gameTime">Objet contenant l'information de temps de jeu de type GameTime</param>
+        public override void Draw(GameTime gameTime)
+        {
+            GestionSprites.Draw(Image, Position, RectangleSource, Color.White);
+        }
+    }
+}
