b658efbafafd45673cc46216061f930c6a222295
Raphaël Brulé <raphael_brule@hotmail.com>
2016-10-05 10:19:55 -0400

    Interface

---
+++ AS2D_2016/AS2D_2016/ICollisionable.cs
@@ -0,0 +1,13 @@
+﻿/* Auteur :            Raphaël Brulé
+   Fichier :           ICollisionable.cs
+   Date :              le 05 octobre 2016
+   Description :       Cette interface représente un objet collisionable.*/
+
+namespace AS2D_2016
+{
+    interface ICollisionable
+    {
+        //À implanter
+        bool EstEnCollision(object autreObjet);
+    }
+}
