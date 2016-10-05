289a5e3757b427d8644fac22746c5488d3c1d67c
Raphaël Brulé <raphael_brule@hotmail.com>
2016-10-05 10:19:39 -0400

    Interface

---
+++ AS2D_2016/AS2D_2016/IDestructible.cs
@@ -0,0 +1,13 @@
+﻿/* Auteur :            Raphaël Brulé
+   Fichier :           IDestructible.cs
+   Date :              le 05 octobre 2016
+   Description :       Cette interface représente un objet destructible.*/
+
+namespace AS2D_2016
+{
+    interface IDestructible
+    {
+        //À implanter
+        bool ÀDétruire { get; }
+    }
+}
